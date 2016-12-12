0bbcc8ff5c9df2c79c8da56ab895534fe4391a18
carlosiff <ocarlosnet@hotmail.com>
2016-12-12 20:48:47 -0200

    Versão Atual 2.2

--- app/src/main/java/br/edu/iff/pooa20162/servicecar/PrincipalActivity.java
+++ app/src/main/java/br/edu/iff/pooa20162/servicecar/PrincipalActivity.java
@@ -1,7 +1,11 @@
 package br.edu.iff.pooa20162.servicecar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class PrincipalActivity extends AppCompatActivity {
 
@@ -9,5 +13,40 @@ public class PrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        ImageButton imageDispo = (ImageButton) findViewById(R.id.imageDispo);
+        imageDispo.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view){
+                Intent intent = new Intent(PrincipalActivity.this,DisponobilidadeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton imageOrca = (ImageButton) findViewById(R.id.imageOrca);
+        imageOrca.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view){
+                Intent intent = new Intent(PrincipalActivity.this,OrcamentoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton imageAvali = (ImageButton) findViewById(R.id.imageAvali);
+        imageAvali.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view){
+                Intent intent = new Intent(PrincipalActivity.this,AvaliacaoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton imageSair = (ImageButton) findViewById(R.id.imageSair);
+        imageSair.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view){
+                Intent intent = new Intent(PrincipalActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
