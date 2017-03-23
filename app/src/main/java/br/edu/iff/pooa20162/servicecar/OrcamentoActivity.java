8d8c45e57c5063fa43a8d1a86c1a6e62ba2f61d5
carlosiff <ocarlosnet@hotmail.com>
2017-03-22 22:09:45 -0300

    Adicionando os metas-datas

--- app/src/main/java/br/edu/iff/pooa20162/servicecar/OrcamentoActivity.java
+++ app/src/main/java/br/edu/iff/pooa20162/servicecar/OrcamentoActivity.java
@@ -2,6 +2,8 @@ package br.edu.iff.pooa20162.servicecar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class OrcamentoActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class OrcamentoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orcamento);
+
+        ListView itens = (ListView) findViewById(R.id.lservicos);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,
+                R.array.servicos, android.R.layout.simple_list_item_1);
+        itens.setAdapter(adapter);
     }
 }
